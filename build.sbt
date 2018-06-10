0c24e8e8aea6289b0ce4d3096e0608f41775644d
blaval <lavalber02@gmail.com>
2018-06-10 12:22:48 +0200

    Straightforward stream

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "akka-stream"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.13"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
+import akka.actor.ActorSystem
+import akka.{Done, NotUsed}
+import akka.stream._
+import akka.stream.scaladsl._
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
 object Main extends App {
-  override def main(args: Array[String]): Unit = {
-    println("Hello spheric bastards!")
-  }
+  implicit val system: ActorSystem = ActorSystem("QuickStart")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  val source: Source[Int, NotUsed] = Source(1 to 100)
+  val done: Future[Done] = source.runForeach(i ⇒ println(i))(materializer)
+
+  done.onComplete(_ ⇒ system.terminate())
 }
